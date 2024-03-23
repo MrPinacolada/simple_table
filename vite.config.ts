7a5f32eb437d6cd7b2e6041206fac37618676d05
MrPinacolada <vasiasko112@gmail.com>
2024-03-23 11:04:47 +0300

    set colors/fonts

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,13 @@ export default defineConfig({
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
     },
+    css: {
+        code: `@font-face {
+            font-family: 'YourFontFamilyName';
+            src: url('/path/to/your/font.woff2') format('woff2'),
+                 url('/path/to/your/font.woff') format('woff');
+            font-weight: normal;
+            font-style: normal;
+        }`,
+    },
 })
