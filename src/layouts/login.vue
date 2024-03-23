3f6ef35407db3a2ca19cab0bff6c0fe9525513cc
MrPinacolada <vasiasko112@gmail.com>
2024-03-23 10:36:11 +0300

    add layouts

---
+++ src/layouts/login.vue
@@ -0,0 +1,13 @@
+<template>
+    <main>
+        <slot></slot>
+    </main>
+</template>
+
+<script lang="ts">
+export default {
+    name: "login-layout",
+}
+</script>
+
+<style scoped lang="scss"></style>
---
+++ src/views/login_page.vue
@@ -0,0 +1,16 @@
+<template>
+    <login_layout>
+        <div></div>
+    </login_layout>
+</template>
+<script lang="ts">
+import login_layout from "@/layouts/login.vue"
+
+export default {
+    name: "login",
+    components: {
+        login_layout,
+    },
+}
+</script>
+<style scoped lang="scss"></style>
