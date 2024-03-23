e4f0ff2ad9be03095d7f0498958e0209cef6dbc3
MrPinacolada <vasiasko112@gmail.com>
2024-03-23 10:24:19 +0300

    add vuex

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import './assets/main.css'
+import "./assets/main.css"
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue"
+import App from "./App.vue"
+import router from "./router"
+import { table_store } from "@/store/table_store"
 
-const app = createApp(App)
+export const app = createApp(App)
 
 app.use(router)
+app.use(table_store)
 
-app.mount('#app')
+app.mount("#app")
---
+++ src/store/table_store.ts
@@ -0,0 +1,11 @@
+import { createStore } from "vuex"
+
+export const table_store = createStore({
+  state() {
+    return {
+      count: 0,
+    }
+  },
+  mutations: {},
+  actions: {},
+})
