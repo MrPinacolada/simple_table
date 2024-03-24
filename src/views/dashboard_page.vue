85e38de9ce3891757f3d8dd994e19fcd12d82613
MrPinacolada <vasiasko112@gmail.com>
2024-03-24 16:59:13 +0300

    добавил немного пространства под таблицей

--- src/views/dashboard_page.vue
+++ src/views/dashboard_page.vue
@@ -255,7 +255,7 @@ export default {
 <style scoped lang="scss">
 .wrapper {
     display: grid;
-    width: 100%;
+    width: 105%;
     height: 100%;
     justify-items: center;
     align-items: start;
